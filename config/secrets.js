ebb588331009a108d597b71224c24c287bdd4250
tmaximini <tmaximini@gmail.com>
2014-05-07 19:33:45 +0200

    remove clutter from secrets

--- config/secrets.js
+++ config/secrets.js
@@ -15,14 +15,6 @@ module.exports = {
     password: process.env.SENDGRID_PASSWORD || 'Your SendGrid Password'
   },
 
-  nyt: {
-    key: process.env.NYT_KEY || 'Your New York Times API Key'
-  },
-
-  lastfm: {
-    api_key: process.env.LASTFM_KEY || 'Your API Key',
-    secret: process.env.LASTFM_SECRET || 'Your API Secret'
-  },
 
   facebookAuth: true,
   facebook: {
@@ -54,53 +46,6 @@ module.exports = {
     clientSecret: process.env.GOOGLE_SECRET || 'Your Client Secret',
     callbackURL: '/auth/google/callback',
     passReqToCallback: true
-  },
-
-  linkedinAuth: false,
-  linkedin: {
-    clientID: process.env.LINKEDIN_ID || 'Your Client ID',
-    clientSecret: process.env.LINKEDIN_SECRET || 'Your Client Secret',
-    callbackURL: '/auth/linkedin/callback',
-    scope: ['r_fullprofile', 'r_emailaddress', 'r_network'],
-    passReqToCallback: true
-  },
-
-  steam: {
-    apiKey: process.env.STEAM_KEY || 'Your Steam API Key'
-  },
-
-  twilio: {
-    sid: process.env.TWILIO_SID || 'Your Twilio SID',
-    token: process.env.TWILIO_TOKEN || 'Your Twilio token'
-  },
-
-  clockwork: {
-    apiKey: process.env.CLOCKWORK_KEY || 'Your Clockwork SMS API Key'
-  },
-
-  tumblr: {
-    consumerKey: process.env.TUMBLR_KEY || 'Your Consumer Key',
-    consumerSecret: process.env.TUMBLR_SECRET || 'Your Consumer Secret',
-    callbackURL: '/auth/tumblr/callback'
-  },
-
-  foursquare: {
-    clientId: process.env.FOURSQUARE_ID || 'Your Client ID',
-    clientSecret: process.env.FOURSQUARE_SECRET || 'Your Client Secret',
-    redirectUrl: process.env.FOURSQUARE_REDIRECT_URL || 'http://localhost:3000/auth/foursquare/callback'
-  },
-
-  venmo: {
-    clientId: process.env.VENMO_ID || 'Your Venmo Client ID',
-    clientSecret: process.env.VENMO_SECRET || 'Your Venmo Client Secret',
-    redirectUrl: process.env.VENMO_REDIRECT_URL || 'http://localhost:3000/auth/venmo/callback'
-  },
-
-  paypal: {
-    host: process.env.PAYPAL_HOST || 'api.sandbox.paypal.com',
-    client_id: process.env.PAYPAL_ID || 'Your Client ID',
-    client_secret: process.env.PAYPAL_SECRET || 'Your Client Secret',
-    returnUrl: process.env.PAYPAL_RETURN_URL || 'http://localhost:3000/api/paypal/success',
-    cancelUrl: process.env.PAYPAL_CANCEL_URL || 'http://localhost:3000/api/paypal/cancel'
   }
+
 };
