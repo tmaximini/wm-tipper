9aed1d5e23bda2e487784fb88631f78423cc7cb7
tmaximini <tmaximini@gmail.com>
2014-05-20 14:20:17 +0200

    design improvements

--- controllers/home.js
+++ controllers/home.js
@@ -1,13 +1,22 @@
+var Group = require('../models/Group');
+
+
 /**
  * GET /
  * Home page.
  */
 
 exports.index = function(req, res) {
+
   if (req.user) {
-    res.render('dashboard/dashboard', {
-      title: 'Dashboard'
+    Group.list({ criteria: { 'members': req.user._id } }, function (err, userGroups) {
+      if (err) return next(err);
+      res.render('dashboard/dashboard', {
+        title: 'Dashboard',
+        userGroups: userGroups
+      });
     });
+
   } else {
     res.render('home', {
       title: 'Das Tippspiel zur Fussball Weltmeisterschaft 2014 in Brasilien'
