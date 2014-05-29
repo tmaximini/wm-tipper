4e9d988783cc8232fe48dd92396e759ea41c191a
tmaximini <tmaximini@gmail.com>
2014-05-30 00:07:16 +0200

    remove video for now

--- public/js/main.js
+++ public/js/main.js
@@ -31,40 +31,40 @@ $(document).ready(function() {
   });
 
   // Find all YouTube videos
-    var $allVideos = $('iframe[src^="//www.youtube.com"]');
+  //var $allVideos = $('iframe[src^="//www.youtube.com"]');
 
-    // The element that is fluid width
-    var $fluidEl = $('.wmTipper');
+  //// The element that is fluid width
+  //var $fluidEl = $('.wmTipper');
 
-    // Figure out and save aspect ratio for each video
-    $allVideos.each(function() {
+  //// Figure out and save aspect ratio for each video
+  //$allVideos.each(function() {
 
-      $(this)
-        .data('aspectRatio', this.height / this.width)
+  //  $(this)
+  //    .data('aspectRatio', this.height / this.width)
 
-        // and remove the hard coded width/height
-        .removeAttr('height')
-        .removeAttr('width');
+  //    // and remove the hard coded width/height
+  //    .removeAttr('height')
+  //    .removeAttr('width');
 
-    });
+  //});
 
-    // When the window is resized
-    // (You'll probably want to debounce this)
-    $(window).resize(function() {
+  //// When the window is resized
+  //// (You'll probably want to debounce this)
+  //$(window).resize(function() {
 
-      var newWidth = $fluidEl.width() - 40;
+  //  var newWidth = $fluidEl.width() - 40;
 
-      // Resize all videos according to their own aspect ratio
-      $allVideos.each(function() {
+  //  // Resize all videos according to their own aspect ratio
+  //  $allVideos.each(function() {
 
-        var $el = $(this);
-        $el
-          .width(newWidth)
-          .height(newWidth * $el.data('aspectRatio'));
+  //    var $el = $(this);
+  //    $el
+  //      .width(newWidth)
+  //      .height(newWidth * $el.data('aspectRatio'));
 
-      });
+  //  });
 
-    // Kick off one resize to fix all videos on page load
-    }).resize();
+  //// Kick off one resize to fix all videos on page load
+  //}).resize();
 
 });
